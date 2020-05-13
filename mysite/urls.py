6a80e1b6c2b567fdfb9faf275cd2249739398b3a
veronica <veronica.alavez@uabc.edu.mx>
2020-05-12 21:06:13 -0700

    cambios en la seccion de comentarios.

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(),name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout', kwargs=
 {'next_page': '/'}),
     path('', include('blog.urls')),
+    
 ]
